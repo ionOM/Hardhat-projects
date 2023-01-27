5400796ad28cad1e9551dbd95b6deff209209abd
Ion <119344918+ionOM@users.noreply.github.com>
2023-01-27 16:51:55 +0100

    Add files via upload

---
+++ Faucet Smart Contract/scripts/deployFaucet.js
@@ -0,0 +1,17 @@
+const hre = require("hardhat");
+
+async function main() {
+  const Faucet = await hre.ethers.getContractFactory("Faucet");
+  const faucet = await Faucet.deploy(
+    "0xD6F400fb2644441d56E1FB9eb1393DB4f74A025b"
+  );
+
+  await faucet.deployed();
+
+  console.log("Facuet contract deployed: ", faucet.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
